b4bbbfbf6c19de27256cb8d73f8df55e24339a5a
E O <114992756+Emmanuel-Awolu60@users.noreply.github.com>
2023-09-27 15:22:31 +0100

    updated

---
+++ main.js
@@ -0,0 +1,11 @@
+ScrollReveal({ reset: true, duration: 2000 });
+
+ScrollReveal().reveal(".roll", {
+  origin: "left",
+  distance: "100px",
+});
+
+ScrollReveal().reveal(".rollpp", {
+  origin: "buttom",
+  distance: "100px",
+});
